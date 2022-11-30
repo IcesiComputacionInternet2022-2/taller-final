57654271ce0f35199c115bd595692ea1a6b48043
Diego Fernando Hidalgo Lopez <1006492807@u.icesi.edu.co>
2022-11-30 15:37:44 -0500

    Create backend project

---
+++ src/main/java/com/edu/icesi/MusicShop/MusicShopApplication.java
@@ -0,0 +1,13 @@
+package com.edu.icesi.MusicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicShopApplication.class, args);
+	}
+
+}
