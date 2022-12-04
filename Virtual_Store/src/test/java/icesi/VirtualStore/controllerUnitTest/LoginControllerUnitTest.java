a874ad152fa31c2f0c9cf98124e9afaa0b2f11fb
Fr4nkC0d3 <61710810+JuanFernandoMartinez@users.noreply.github.com>
2022-12-03 19:41:06 -0500

    Login Controller unit test created

--- Virtual_Store/src/test/java/icesi/VirtualStore/controllerUnitTest/LoginControllerUnitTest.java
+++ Virtual_Store/src/test/java/icesi/VirtualStore/controllerUnitTest/LoginControllerUnitTest.java
@@ -1,4 +1,60 @@
 package icesi.VirtualStore.controllerUnitTest;
 
+import icesi.VirtualStore.controller.LoginController;
+import icesi.VirtualStore.dto.LoginDTO;
+import icesi.VirtualStore.error.exception.VirtualStoreException;
+import icesi.VirtualStore.service.LoginService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
 public class LoginControllerUnitTest {
+    /*
+     * items to test
+     * Login Method
+     */
+
+    @Mock
+    private LoginService loginService;
+
+    @InjectMocks
+    private LoginController loginController;
+
+
+    @Test
+    public void testEmailLogin(){
+        LoginDTO loginDTOA = new LoginDTO("jm1811324@gmail.com", "M00n_W4lk3r");
+
+        loginController.login(loginDTOA);
+        verify(loginService, times(1)).loginByEmail(loginDTOA);
+    }
+
+    @Test
+    public void  testPhoneLogin(){
+        LoginDTO loginDTOB = new LoginDTO("+573154175103", "M00n_W4lk3r");
+
+        loginController.login(loginDTOB);
+        verify(loginService,times(1)).loginByPhoneNumber(loginDTOB);
+
+    }
+
+    @Test
+    public void testWrongLogin(){
+        LoginDTO loginDTOC = new LoginDTO("abcdeslkaslkjdf", "M00n_W4lk3r");
+
+        Assertions.assertThrows(VirtualStoreException.class, ()->{
+            loginController.login(loginDTOC);
+        });
+    }
+
+
 }
